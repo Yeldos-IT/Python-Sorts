67c50e3635d76f9568f358c78304007134cfd1a5
Yeldos <161858786+Yeldos-IT@users.noreply.github.com>
2024-08-02 17:00:40 +0500

    Add files via upload

---
+++ Bubble sort.py
@@ -0,0 +1,14 @@
+def BubbleSort(arr):
+    n = len(arr)
+    for i in range(n):
+        swapped = False
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j] , arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+        if (swapped == False):
+                break
+arr = [64, 34, 25, 12, 22, 11, 90]
+BubbleSort(arr)
+for i in range(len(arr)):
+    print("%d" % arr[i], end=" ")
